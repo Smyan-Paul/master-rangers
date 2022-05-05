fb351a7f58a654bfcdcd8f1e791b70ca4235dc3d
Smyan-Paul <87228812+Smyan-Paul@users.noreply.github.com>
2022-05-05 16:29:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,18 @@ var block_image_object= "";
 
 function new_image(get_image)
 {
-	// to upload images
+	fabric.Image.fromURL(get_image , function(Img){
+	      block_image_object = Img;
+		
+	       block_image_object.scaleToWidth(block_image_width);
+	      block_image_object.scaleToHeight(block_image_height);
+		block_image_object.set({
+		  top:0,
+			left:0
+		});
+		canavas.add(block_image_object)
+	});
+	
 }
 
 window.addEventListener("keydown", my_keydown);
@@ -19,30 +30,35 @@ function my_keydown(e)
 keyPressed = e.keyCode;
 console.log(keyPressed);
 
-	if(keyPressed == '?') // add appropriate keycode
+	if(keyPressed == '82') // add appropriate keycode
 	{
-		// upload red ranger
+		new_image(rr1.png);
+		console.log('r')
 	}
 	if(keyPressed == '71')
 	{
 		block_x = 200;
-		// upload green ranger
+		new_image(gr.png);
+		console.log('g')
 	}
 	
 	if(keyPressed == '89')
 	{
 		block_x =350;
-		// upload yellow ranger
+		new_image(yr.png);
+		console.log('y')
 	}
 	if(keyPressed == '80')
 	{
 		block_x = 600;
-		// upload pink ranger
+		new_image(pr.png);
+		console.log('p')
 	}
 	if(keyPressed == '66')
 	{
 		block_x = 700;
-	// upload blue ranger
+	new_image(br.png);
+		console.log('b')
 	}
 	
 }
